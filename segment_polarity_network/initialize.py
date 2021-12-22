51f21e8e7ec1399e7798764e3c49810c4a4ba3f2
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2021-12-22 14:12:45 -0600

    Using multisource

--- segment_polarity_network/initialize.py
+++ segment_polarity_network/initialize.py
@@ -1,6 +1,6 @@
 from itertools import permutations, product
 from segment_polarity_model import SegmentPolarityModel
-from mc_boomer.action import Action, Source
+from mc_boomer.action import Action, MultiSource
 
 def emptyModel():
     num_cells = 4
@@ -30,35 +30,35 @@ def allActions():
 
     actions = set()
     for src,dst in internal_rules:
-        actions.add(Action(srcs=(Source(span='i', src=src),), type='a', dst=dst))
-        actions.add(Action(srcs=(Source(span='i', src=src),), type='i', dst=dst))
+        actions.add(Action(srcs=(MultiSource(span='i', src=src),), type='a', dst=dst))
+        actions.add(Action(srcs=(MultiSource(span='i', src=src),), type='i', dst=dst))
 
     for src,dst in transcription_factor_rules:
-        actions.add(Action(srcs=(Source(span='i', src=src),), type='a', dst=dst))
-        actions.add(Action(srcs=(Source(span='i', src=src),), type='i', dst=dst))
+        actions.add(Action(srcs=(MultiSource(span='i', src=src),), type='a', dst=dst))
+        actions.add(Action(srcs=(MultiSource(span='i', src=src),), type='i', dst=dst))
 
     for src,dst in external_rules:
-        actions.add(Action(srcs=(Source(span='e', src=src),), type='a', dst=dst))
-        actions.add(Action(srcs=(Source(span='e', src=src),), type='i', dst=dst))
+        actions.add(Action(srcs=(MultiSource(span='e', src=src),), type='a', dst=dst))
+        actions.add(Action(srcs=(MultiSource(span='e', src=src),), type='i', dst=dst))
 
     # Create compound actions
     # We choose the compounds assuming prior knowledge that only the species
     # form active compounds with each other
-    compounds = [(Source('CIA','i'), Source('SLP','i')), 
-                 (Source('CI','i'),  Source('SMO','i')),
-                 (Source('CI','i'),  Source('HH','e')),
+    compounds = [(MultiSource('CIA','i'), MultiSource('SLP','i')), 
+                 (MultiSource('CI','i'),  MultiSource('SMO','i')),
+                 (MultiSource('CI','i'),  MultiSource('HH','e')),
                  ]
                  # Already set as an action below
-                 #(Source('PTC','i'), Source('HH','e')),
+                 #(MultiSource('PTC','i'), MultiSource('HH','e')),
     for compound,dst in list(product(compounds, genes)) + list(product(compounds, proteins)):
         actions.add(Action(srcs=compound, dst=dst, type='i'))
         actions.add(Action(srcs=compound, dst=dst, type='a'))
 
     wg_actions = [
-                 Action(srcs=(Source('CIA','i'), Source('wg','i')), dst='wg', type='a'),
-                 Action(srcs=(Source('CIA','i'), Source('wg','i')), dst='wg', type='i'),
-                 Action(srcs=(Source('SLP','i'), Source('wg','i')), dst='wg', type='a'),
-                 Action(srcs=(Source('SLP','i'), Source('wg','i')), dst='wg', type='i'),
+                 Action(srcs=(MultiSource('CIA','i'), MultiSource('wg','i')), dst='wg', type='a'),
+                 Action(srcs=(MultiSource('CIA','i'), MultiSource('wg','i')), dst='wg', type='i'),
+                 Action(srcs=(MultiSource('SLP','i'), MultiSource('wg','i')), dst='wg', type='a'),
+                 Action(srcs=(MultiSource('SLP','i'), MultiSource('wg','i')), dst='wg', type='i'),
                  ]
     for action in wg_actions:
         actions.add(action)
@@ -80,19 +80,19 @@ def initialState():
 
     # Initialize the genes to be activating the proteins.
     # We can assume this is true apriori
-    gene_protein_rules = [Action(srcs=(Source(span='i',src='en'),), type='a', dst='EN'),
-                          Action(srcs=(Source(span='i',src='wg'),), type='a', dst='WG'),
-                          Action(srcs=(Source(span='i',src='ci'),), type='a', dst='CI'),
-                          Action(srcs=(Source(span='i',src='ptc'),), type='a',dst='PTC'),
-                          Action(srcs=(Source(span='i',src='hh'),), type='a', dst= 'HH')]
+    gene_protein_rules = [Action(srcs=(MultiSource(span='i',src='en'),), type='a', dst='EN'),
+                          Action(srcs=(MultiSource(span='i',src='wg'),), type='a', dst='WG'),
+                          Action(srcs=(MultiSource(span='i',src='ci'),), type='a', dst='CI'),
+                          Action(srcs=(MultiSource(span='i',src='ptc'),), type='a',dst='PTC'),
+                          Action(srcs=(MultiSource(span='i',src='hh'),), type='a', dst= 'HH')]
 
     # Initialize CI to activate CIA and CIR, because these are truncated CI protein
-    ci_rules = [Action(srcs=(Source(span='i',src='CI'),), type='a', dst='CIA'),
-                Action(srcs=(Source(span='i',src='CI'),), type='a', dst='CIR')]
+    ci_rules = [Action(srcs=(MultiSource(span='i',src='CI'),), type='a', dst='CIA'),
+                Action(srcs=(MultiSource(span='i',src='CI'),), type='a', dst='CIR')]
 
     # Initialize PTC and HH (external) to activate PH, because its a complex
     # Maybe we could remove PH entirely because it's just a compound??
-    ph_rule = [Action(srcs=(Source('PTC','i'), Source('HH','e')), dst ='PH', type='a')]
+    ph_rule = [Action(srcs=(MultiSource('PTC','i'), MultiSource('HH','e')), dst ='PH', type='a')]
 
     # The rules below aren't explicitly prior knowledge
     # They just represent some weird interactions that don't fit the "normal"
@@ -102,15 +102,15 @@ def initialState():
     # So we just add them directly. This could be relaxed, but then we'd have to add a bunch of 
     # additional actions that include one or both terms negated. That would explode the search 
     # space. 
-    ptc_rule = [Action(srcs=(Source('PTC','i'), Source('~HH','e')), dst='PTC', type='a')]
-    smo_rule = [Action(srcs=(Source('~PTC','i'),), dst='SMO', type='a')]
+    ptc_rule = [Action(srcs=(MultiSource('PTC','i'), MultiSource('~HH','e')), dst='PTC', type='a')]
+    smo_rule = [Action(srcs=(MultiSource('~PTC','i'),), dst='SMO', type='a')]
 
     # These are weird interactions in Reka's model where a membrane protein can directly
     # activate/inhibit internal proteins in another cell
     # Should these be prior knowledge?
     external_internal_rules = [
-        Action(srcs=(Source(src='HH', span='e'),), dst='CIR', type='i'),
-        Action(srcs=(Source(src='WG', span='e'),), dst='en', type='a')
+        Action(srcs=(MultiSource(src='HH', span='e'),), dst='CIR', type='i'),
+        Action(srcs=(MultiSource(src='WG', span='e'),), dst='en', type='a')
     ]
 
     # Aggregate all the rules that we manually specify
